3a536b7c1cf1061ec841b06a060ec929c816160c
Lucas Pereira <lucaspereiraribeir123@gmail.com>
2022-12-14 21:40:18 -0300

    Curso parte 2 - componente de cartão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CartoaComponent } from './componentes/cartoa/cartoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartoaComponent
   ],
   imports: [BrowserModule,
     AppRoutingModule,
