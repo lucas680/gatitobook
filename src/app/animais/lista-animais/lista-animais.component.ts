70ba27008f626d94bfe43253275eef1af4f79a54
Lucas Pereira <lucaspereiraribeir123@gmail.com>
2022-12-15 19:23:11 -0300

    Lista de animais

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AnimaisService } from './../animais.service';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { Animais } from './../animais';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
   styleUrls: ['./lista-animais.component.css']
 })
-export class ListaAnimaisComponent {
+export class ListaAnimaisComponent implements OnInit{
 
+  animais!: Animais
+
+  constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService){}
+
+  ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario)=>{
+      const userName = usuario.name ?? ''
+      this.animaisService.listaDoUsuario(userName).subscribe((animais)=>{
+        this.animais = animais
+      })
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CartoaComponent } from './componentes/cartoa/cartoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CartoaComponent
+    AppComponent
   ],
   imports: [BrowserModule,
     AppRoutingModule,
